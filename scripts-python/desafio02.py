4b9e11add41ce7c36e564fbef5a917adad4fc2af
Iago Lima <iagoclima22@gmail.com>
2025-07-02 14:59:54 -0300

    Add files via upload

---
+++ scripts-python/desafio02.py
@@ -0,0 +1,4 @@
+dia = input('Qual dia do mês você nasceu? ')
+mes = input('Qual é o mês do seu nascimento? ')
+ano = input('Qual é o ano do seu nascimento? ')
+print('Você nasceu no dia ', dia, ' de ', mes, ' de ', ano, '. Correto?')
