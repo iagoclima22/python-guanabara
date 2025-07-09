2d2c8201df5d3332b836878db248cbeb92de1abe
Iago Lima <iagoclima22@gmail.com>
2025-07-09 14:39:08 -0300

    Add files via upload

---
+++ PythonExercicios/ex006.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um número: '))
+dobro = 2 * n
+triplo = 3 * n
+raiz2 = n**2
+print('Dobro = {}'.format(dobro))
+print('Triplo = {}'.format(triplo))
+print('Raiz quadrada = {}'.format(raiz2))
