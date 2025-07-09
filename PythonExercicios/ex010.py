ceaffcdd58fc055d86be2575ba5657c5d4f28f6a
Iago Lima <iagoclima22@gmail.com>
2025-07-09 15:01:15 -0300

    Add files via upload

---
+++ PythonExercicios/ex010.py
@@ -0,0 +1,3 @@
+dinheiroReais = float(input('Digite quantos reais você tem em sua carteira: R$'))
+reaisParaDolares = dinheiroReais * 3.27 #cotação na época do exercício
+print(f'Você pode comprar US${reaisParaDolares}')
