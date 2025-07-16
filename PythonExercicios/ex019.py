ad421407c32a94d4bb630730da1cdebfc2d170df
Iago Lima <iagoclima22@gmail.com>
2025-07-16 14:20:54 -0300

    Add files via upload

---
+++ PythonExercicios/ex019.py
@@ -0,0 +1,9 @@
+from random import choice
+aluno1 = input("Primeiro aluno: ")
+aluno2 = input("Segundo aluno: ")
+aluno3 = input("Terceiro aluno: ")
+aluno4 = input("Quarto aluno: ")
+
+lista = [aluno1, aluno2, aluno3, aluno4]
+sorteado = choice(lista)
+print('O aluno escolhido foi {}'.format(sorteado))
