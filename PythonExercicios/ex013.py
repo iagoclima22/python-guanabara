bffd8e6f1f7bbd5e630af51ca3605950ca62385e
Iago Lima <iagoclima22@gmail.com>
2025-07-16 09:47:16 -0300

    Update ex013.py

--- PythonExercicios/ex013.py
+++ PythonExercicios/ex013.py
@@ -1,4 +1,4 @@
 salarioAtual = float(input('Digite o salário atual de um fincionário: '))
 aumento = salarioAtual * 15/100
 novoSalario = salarioAtual + aumento
-print(f'O novo salário do fincionário com um aumento de 15% é igual a R${novoSalario:.2f}')
+print(f'O novo salário do funcionário com um aumento de 15% é igual a R${novoSalario:.2f}')
